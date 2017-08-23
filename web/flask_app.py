853a09ce767783b450d92b4171a7ac4f09e22a4e
Will Greenway <wpgreenway@gmail.com>
2017-08-23 18:57:19 -0500

    Actually convert uploaded PDFs and provide them as attachments (named with the current date)
    * Also some small display tweaks for the front-end
    - Show error messages in red
    - Use minimal "milligram" css to give a better overall feel

--- web/flask_app.py
+++ web/flask_app.py
@@ -1,34 +1,47 @@
-from flask import Flask, request, redirect, flash, render_template, request, url_for
+from flask import Flask, Response, request, redirect, flash, render_template, url_for
+from openpyxl.writer.excel import save_virtual_workbook
 
+import lib.Automation as Automation
+
+EXCEL_MIMETYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
 app.secret_key = 'thatsagoodasecret'
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def build_filename():
+    date_str = datetime.datetime.now().strftime("%Y%m%d")
+    return date_str + "_Kids_Sheet.xlsx"
+
 
 @app.route('/', methods=['GET'])
 def show_form():
     return render_template('index.html')
 
+
 @app.route('/', methods=['POST'])
 def upload_file():
-    # check if the post request has the file part
+    redirect_response = redirect(request.url)
     if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
+        flash('No file provided')
+        return redirect_response
 
     file = request.files['file']
-    # if user does not select file, browser also
-    # submit a empty part without filename
     if file.filename == '':
         flash('No selected file')
-        return redirect(request.url)
+        return redirect_response
+
     if not allowed_file(file.filename):
         flash('File type not supported. Please provide a PDF.')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        flash('Got file: ' + file.filename)
-        return redirect(request.url)
+        return redirect_response
+
+    wb = Automation.format_pdf_to_excel(file)
+    return Response(save_virtual_workbook(wb),
+                    mimetype=EXCEL_MIMETYPE,
+                    headers={"Content-Disposition": "attachment;filename=" + build_filename()})
