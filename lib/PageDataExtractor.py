9260d3e09a18898327cff6304e93a57d4a9296d6
Will Greenway <wpgreenway@gmail.com>
2017-08-23 22:12:11 -0500

    Extract new "CellData" class for managing information during PageDataExtractor
    * Encapsulates logic for what cells can be ignored, how to parse times, etc.

--- lib/PageDataExtractor.py
+++ lib/PageDataExtractor.py
@@ -21,20 +21,20 @@ NEED TO MAKE SURE THAT WE ALWAYS FOLLOW THESE RULES:
 
 
 def process_text_pages(text_pages):
-    regex_list = []
+    data = []
     change_property = 0
     for text_page in text_pages:
-        page_regex_list = __process_page(change_property, text_page)
-        regex_list.extend(page_regex_list)
+        page_data = __process_page(change_property, text_page)
+        data.extend(page_data)
 
-    print(regex_list)
+    print(data)
     # Taking the attributes and adding 1 to the odd numbers so that they match the person in the join later
-    for item in regex_list:
+    for item in data:
         if item[0] == 'Status' and item[3] & 1:  # x & 1 -- if True, then number is odd, else false
             item[0] = 'Room'
             item[3] += 1
 
-    return regex_list
+    return data
 
 
 """
@@ -55,31 +55,20 @@ def __process_page(change_property, text_page):
     counter = 0
     change_count = 0
     previous_row_type = ''
-    page_regex_list = []
+    page_data = []
 
     for text in text_page.text.splitlines():
-        if text == "":  # If it's blank, skip it
+        cell_text = CellText(text)
+
+        if cell_text.should_skip():
             continue
-        if re.search('Regulars', text):  # If it has "Regulars", it's an aggregate count row, so skip it
-            continue
-        if re.search('Sunday Morning - ',
-                     text):  # It's the title row "Sunday Morning - <date>", therefore skip
-            continue
-        if re.search('Check-Ins', text):  # Take's out the "2 Check-Ins" property that we don't want
-            continue
-        if re.search('\d\d:\d\d[ap]',
-                     text):  # If it has the time, reset the counter, create the time_value to pass on
+
+        if cell_text.get_time():
+            # If it has the time, reset the counter, create the time_value to pass on
             counter = 0
-            time_value = re.search('\d\d:\d\d[ap]', text)
-            time_value = time_value.group(0)
-        elif re.search('\d:\d\d[ap]',
-                       text):  # If it has the time, reset the counter, create the time_value to pass on
-            counter = 0
-            time_value = re.search('\d:\d\d[ap]', text)
-            time_value = time_value.group(0)
-        elif re.search(',',
-                       text):  # If it has a comma, it's a name, so add 2 to the counter (2 elements to match)
-            if previous_row_type == "Status":  # previous_marker == 0:  # If the preceeding element was a Status and not a person,
+            time_value = cell_text.get_time()
+        elif cell_text.is_person():
+            if previous_row_type == "Status":  # If the preceding element was a Status and not a person,
                 counter = 0  # reset the counter (new page)
                 change_count += 1
 
@@ -87,11 +76,13 @@ def __process_page(change_property, text_page):
                 change_count = 0
                 change_property += 1
 
+            # It's a name, so add 2 to the counter (2 elements to match)
             counter += 2
             previous_row_type = "Person"
-            page_regex_list.append(("Person", page, change_property, counter, text, time_value))
+            page_data.append(("Person", page, change_property, counter, text, time_value))
 
-        else:  # Otherwise, simply add one to the counter. Name counter = 2nd value's counter, and 1st values's counter - 1
+        else:
+            # Otherwise, simply add one to the counter. Name counter = 2nd value's counter, and 1st values's counter - 1
             if previous_row_type != "Status":
                 previous_row_type = "Status"
                 change_count += 1
@@ -101,15 +92,33 @@ def __process_page(change_property, text_page):
                     change_property += 1
 
                 counter = 0
-                counter += 1
 
-                page_regex_list.append(["Status", page, change_property, counter, text, time_value])
-            else:
-                counter += 1
-                page_regex_list.append(["Status", page, change_property, counter, text, time_value])
-                # Problem with a page where there isn't a header, there isn't a clean event break, to start counting by 1's
-                # and then pairing up the attributes with the people.
-                # Potential solution = if the preceding value had a comma, and this one doesn't, reset the counter
-                # Affirmative - This solved it.
+            counter += 1
+            page_data.append(["Status", page, change_property, counter, text, time_value])
 
-    return page_regex_list
+    return page_data
+
+
+class CellText:
+    def __init__(self, text):
+        self.text = text
+
+    def should_skip(self):
+        return (
+            self.text == "" or
+            re.search('Regulars', self.text) or  # If it has "Regulars", it's an aggregate count row, so skip it
+            re.search('Sunday Morning - ', self.text) or  # It's the title row "Sunday Morning - <date>", therefore skip
+            re.search('Check-Ins', self.text)  # Take's out the "2 Check-Ins" property that we don't want
+        )
+
+    def get_time(self):
+        long_form_time = re.search('\d\d:\d\d[ap]', self.text)
+        if long_form_time:
+            return long_form_time.group(0)
+
+        short_form_time = re.search('\d:\d\d[ap]', self.text)
+        if short_form_time:
+            return short_form_time.group(0)
+
+    def is_person(self):
+        return re.search(',', self.text)
