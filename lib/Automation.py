9f3cb5173278bbb45e921c1fd62df23c9889fe88
Will Greenway <wpgreenway@gmail.com>
2017-08-23 20:48:47 -0500

    Return a list of "text page" objects from PdfScraper instead of the full text
    * This allows for saving the page number along with the text of that page in the object in memory instead of encoding that information back into the text
    * Now the Automation function can just iterate over the pages instead of relying on a regex to find the page splits

--- lib/Automation.py
+++ lib/Automation.py
@@ -1,9 +1,3 @@
-"""
-
-Mining Checkout PDF and exporting proper file
-
-"""
-
 import re
 
 import openpyxl
@@ -11,126 +5,38 @@ import pandas as pd
 from openpyxl.styles import Font
 from openpyxl.utils.dataframe import dataframe_to_rows
 
-from lib.PdfScraper import PdfScraper
-
-# print(convert_pdf_to_txt("C:\\Users\\Michael Chapman\\Downloads\\Mike Test - Check-Ins Report.pdf"))
+import lib.PdfScraper as PdfScraper
 
 """
 
-Function that takes the raw PDF scraped string and iterates line by line to grab only the relevant information:
+Function that takes the pages of text and iterates line by line to grab only the relevant information:
 (Person, Attributes about that person like Service Time and Location)
+
+References:
+
+Returning the regular expression match -- .group(0)
+https://stackoverflow.com/questions/18493677/how-do-i-return-a-string-from-a-regex-match-in-python
+
+How to iterate through the lines of the long single text string with /n line breaks
+https://stackoverflow.com/questions/22918013/python-iterate-over-a-string-containing-newlines
+
+NEED TO MAKE SURE THAT WE ALWAYS FOLLOW THESE RULES:
+    NEVER PUT A COMMA IN A ROOM NAME OR ROLE NAME, NEVER PUT "Regulars" IN A ROOM NAME OR A ROLE NAME
+    NEVER PUT "Sunday Morning - " IN A ROOM NAME OR ROLE NAME
 
 """
 
 
 def format_pdf_to_excel(input_file):
-    pdftext = PdfScraper().convert_pdf_to_txt(input_file)
-
-    # Returning the regular expression match -- .group(0)
-    # https://stackoverflow.com/questions/18493677/how-do-i-return-a-string-from-a-regex-match-in-python
-
-    # How to iterate through the lines of the long single text string with /n line breaks
-    # https://stackoverflow.com/questions/22918013/python-iterate-over-a-string-containing-newlines
-
-    """
-    Lines go from upper left of page in columns all the way down until the last name on the page,
-    then it goes to the top of the page on the right side and down that column,
-    and finally it goes through the middle of the page (2 check ins).
-
-    Seems to be doubling the number of pages per single instance of "2 check ins" (2 pages = 4 pages)
-    Pages without "2 check ins" don't seem to duplicate
-
-    counter = The way we count which propery goes with which person. This will reset upon a page change
-
-    """
+    text_pages = PdfScraper.convert_pdf_to_txt(input_file)
 
     regex_list = []
-    counter = 0
-    row_type = ""
-    previous_row_type = ""
-    page = 0
-
-    change_count = 0
     change_property = 0
+    for text_page in text_pages:
+        page_regex_list = __process_page(change_property, text_page)
+        regex_list.extend(page_regex_list)
 
-    pdftext_split = pdftext.splitlines()
-
-    # Loop is in order by time increasing, and spits out key lines:
-    # NEED TO MAKE SURE THAT WE ALWAYS FOLLOW THESE RULES:
-    # NEVER PUT A COMMA IN A ROOM NAME OR ROLE NAME, NEVER PUT "Regulars" IN A ROOM NAME OR A ROLE NAME
-    # NEVER PUT "Python" or "Python Page:" IN A ROOM NAME OR ROLE NAME
-    # NEVER PUT "Sunday Morning - " IN A ROOM NAME OR ROLE NAME
-
-    for i in range(len(pdftext_split)):
-
-        text = pdftext_split[i]  # String/text of the individual line
-
-        if re.search('Python Page:', text):
-            page = re.search('\d{1,}', text)  # Pulls out the number of the page (can be multiple digits)
-            page = int(page.group(0))
-            counter = 0
-            change_count = 0
-            row_type = ''
-            previous_row_type = ''
-            continue
-        if text == "":  # If it's blank, skip it
-            continue
-        if re.search('Regulars', text):  # If it has "Regulars", it's an aggregate count row, so skip it
-            # counter = 0
-            continue
-        if re.search('Sunday Morning - ', text):  # It's the title row "Sunday Morning - <date>", therefore skip
-            # counter = 0
-            continue
-        if re.search('Check-Ins', text):  # Take's out the "2 Check-Ins" property that we don't want
-            # counter = 0
-            continue
-        if re.search('\d\d:\d\d[ap]', text):  # If it has the time, reset the counter, create the time_value to pass on
-            counter = 0
-            time_value = re.search('\d\d:\d\d[ap]', text)
-            time_value = time_value.group(0)
-            # regex_list.append((i, counter, text, time_value))
-        elif re.search('\d:\d\d[ap]', text):  # If it has the time, reset the counter, create the time_value to pass on
-            counter = 0
-            time_value = re.search('\d:\d\d[ap]', text)
-            time_value = time_value.group(0)
-            # regex_list.append((i, counter, text, time_value))
-        elif re.search(',', text):  # If it has a comma, it's a name, so add 2 to the counter (2 elements to match)
-            row_type = "Person"
-            if previous_row_type == "Status":  # previous_marker == 0:  # If the preceeding element was a Status and not a person,
-                counter = 0  # reset the counter (new page)
-                change_count += 1
-
-            if change_count > 1:
-                change_count = 0
-                change_property += 1
-
-            counter += 2
-            previous_row_type = "Person"
-            regex_list.append(("Person", page, change_property, counter, text, time_value))
-
-        else:  # Otherwise, simply add one to the counter. Name counter = 2nd value's counter, and 1st values's counter - 1
-            row_type = "Status"
-            if previous_row_type != row_type:
-                previous_row_type = "Status"
-                change_count += 1
-
-                if change_count > 1:
-                    change_count = 0
-                    change_property += 1
-
-                counter = 0
-                counter += 1
-
-                regex_list.append(["Status", page, change_property, counter, text, time_value])
-            else:
-                counter += 1
-                regex_list.append(["Status", page, change_property, counter, text,
-                                   time_value])  # Has to be brackets so we can edit later
-                # Problem with a page where there isn't a header, there isn't a clean event break, to start counting by 1's
-                # and then pairing up the attributes with the people.
-                # Potential solution = if the preceding value had a comma, and this one doesn't, reset the counter
-                # Affirmative - This solved it.
-
+    print(regex_list)
     # Taking the attributes and adding 1 to the odd numbers so that they match the person in the join later
     for item in regex_list:
         if item[0] == 'Status' and item[3] & 1:  # x & 1 -- if True, then number is odd, else false
@@ -250,40 +156,74 @@ def format_pdf_to_excel(input_file):
 
 
 """
+Lines go from upper left of page in columns all the way down until the last name on the page,
+then it goes to the top of the page on the right side and down that column,
+and finally it goes through the middle of the page (2 check ins).
 
-Old code trying to use the PDF object types, but the line-by-line scraping works fine:
+Seems to be doubling the number of pages per single instance of "2 check ins" (2 pages = 4 pages)
+Pages without "2 check ins" don't seem to duplicate
 
-
-This shows the type of object and LTTextBoxHorizonal had the data I was looking for,
-however there the first person value would also be lumped in with the title value, so we probably need to loop
-line by line like before
-
-path = "C:\\Users\\Michael Chapman\\Downloads\\Mike Test - Check-Ins Report.pdf"
-
-rsrcmgr = PDFResourceManager()
-retstr = StringIO()
-codec = 'utf-8'
-laparams = LAParams()
-device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-fp = open(path, 'rb')  # file(path, 'rb')  # file() was Python 2.x, open is Python 3.x
-interpreter = PDFPageInterpreter(rsrcmgr, device)
-password = ""
-maxpages = 0
-caching = True
-pagenos = set()
-fstr = ''
-for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
-                              check_extractable=True):
-    interpreter.process_page(page)
-    layout = device.get_result()
-    for element in layout:
-        print(element)
-
-str = retstr.getvalue()
-fstr += str
-
-fp.close()
-device.close()
-retstr.close()
+counter = The way we count which propery goes with which person. This will reset upon a page change
 
 """
+
+
+def __process_page(change_property, text_page):
+    page = text_page.page_number
+    counter = 0
+    change_count = 0
+    previous_row_type = ''
+    page_regex_list = []
+
+    for text in text_page.text.splitlines():
+        if text == "":  # If it's blank, skip it
+            continue
+        if re.search('Regulars', text):  # If it has "Regulars", it's an aggregate count row, so skip it
+            continue
+        if re.search('Sunday Morning - ', text):  # It's the title row "Sunday Morning - <date>", therefore skip
+            continue
+        if re.search('Check-Ins', text):  # Take's out the "2 Check-Ins" property that we don't want
+            continue
+        if re.search('\d\d:\d\d[ap]', text):  # If it has the time, reset the counter, create the time_value to pass on
+            counter = 0
+            time_value = re.search('\d\d:\d\d[ap]', text)
+            time_value = time_value.group(0)
+        elif re.search('\d:\d\d[ap]', text):  # If it has the time, reset the counter, create the time_value to pass on
+            counter = 0
+            time_value = re.search('\d:\d\d[ap]', text)
+            time_value = time_value.group(0)
+        elif re.search(',', text):  # If it has a comma, it's a name, so add 2 to the counter (2 elements to match)
+            if previous_row_type == "Status":  # previous_marker == 0:  # If the preceeding element was a Status and not a person,
+                counter = 0  # reset the counter (new page)
+                change_count += 1
+
+            if change_count > 1:
+                change_count = 0
+                change_property += 1
+
+            counter += 2
+            previous_row_type = "Person"
+            page_regex_list.append(("Person", page, change_property, counter, text, time_value))
+
+        else:  # Otherwise, simply add one to the counter. Name counter = 2nd value's counter, and 1st values's counter - 1
+            if previous_row_type != "Status":
+                previous_row_type = "Status"
+                change_count += 1
+
+                if change_count > 1:
+                    change_count = 0
+                    change_property += 1
+
+                counter = 0
+                counter += 1
+
+                page_regex_list.append(["Status", page, change_property, counter, text, time_value])
+            else:
+                counter += 1
+                page_regex_list.append(["Status", page, change_property, counter, text, time_value])
+                # Problem with a page where there isn't a header, there isn't a clean event break, to start counting by 1's
+                # and then pairing up the attributes with the people.
+                # Potential solution = if the preceding value had a comma, and this one doesn't, reset the counter
+                # Affirmative - This solved it.
+
+    return page_regex_list
--- lib/PdfScraper.py
+++ lib/PdfScraper.py
@@ -6,28 +6,28 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 
 
-class PdfScraper:
-    def __init__(self):
-        return
+def convert_pdf_to_txt(input_file):
+    pages = PDFPage.get_pages(input_file, set(), maxpages=0, password="", caching=True, check_extractable=True)
 
-    def convert_pdf_to_txt(self, input_file):
-        pages = PDFPage.get_pages(input_file, set(), maxpages=0, password="", caching=True, check_extractable=True)
+    resource_manager = PDFResourceManager()
+    retstr = StringIO()
+    text_converter = TextConverter(resource_manager, retstr, codec='utf-8', laparams=(LAParams()))
+    interpreter = PDFPageInterpreter(resource_manager, text_converter)
 
-        resource_manager = PDFResourceManager()
-        retstr = StringIO()
-        text_converter = TextConverter(resource_manager, retstr, codec='utf-8', laparams=(LAParams()))
-        interpreter = PDFPageInterpreter(resource_manager, text_converter)
+    try:
+        text_pages = []
+        for page_number, page in enumerate(pages):
+            interpreter.process_page(page)
+            page_text = retstr.getvalue()
+            text_pages.append(TextPage(page_number, page_text))
+        return text_pages
 
-        try:
-            pdf_as_string = ''
-            for page_number, page in enumerate(pages):
-                interpreter.process_page(page)
-                page_text = retstr.getvalue()
-                pdf_as_string += "Python Page:" + str(page_number) + " newline \n"
-                pdf_as_string += page_text
+    finally:
+        text_converter.close()
+        retstr.close()
 
-            return pdf_as_string
 
-        finally:
-            text_converter.close()
-            retstr.close()
+class TextPage:
+    def __init__(self, page_number, text):
+        self.page_number = page_number
+        self.text = text
