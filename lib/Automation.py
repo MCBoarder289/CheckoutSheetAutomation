2668529c2c3acb9c2ee051ecb8b709ce9c42d004
Michael Chapman <chapman.michael.c@gmail.com>
2017-11-13 21:58:59 -0600

    Adding more comments to make things clearer

--- lib/Automation.py
+++ lib/Automation.py
@@ -3,9 +3,20 @@ import lib.PageDataExtractor as PersonDataExtractor
 import lib.PdfScraper as PdfScraper
 import lib.WorkbookBuilder as WorkbookBuilder
 
+# Import statements go to the folder.code, so lib.PdfScraper allows us to use code in the PdfScraper.py file
+
+
+# This file defines the full end-to-end function of taking the PDF and turning it into the formatted excel sheet
+# so that the team can print out the roster.
+
 
 def format_pdf_to_excel(input_file):
     text_pages = PdfScraper.convert_pdf_to_txt(input_file)
     page_data = PersonDataExtractor.process_text_pages(text_pages)
     tabs, tabs_df_list = DataFrameBuilder.build_data_frame(page_data)
     return WorkbookBuilder.build_workbook(tabs, tabs_df_list)
+
+# First, we scrape the PDF for text and output that text
+# Then we loop through the text/PDF pages to extract the data we need, and output that data
+# Next we create dataframes with the proper structure, filtered for just who we need and divided into tabs
+# Finally, we create an excel workbook from the dataframes that matches the spaced format we need
--- lib/DataFrameBuilder.py
+++ lib/DataFrameBuilder.py
@@ -20,22 +20,22 @@ https://chrisalbon.com/python/pandas_index_select_and_filter.html
 
 def build_data_frame(page_data):
     labels = ['RowType', 'Page', 'Change_Property', 'Counter', 'Value', 'Time']
-    df = pd.DataFrame.from_records(page_data, columns=labels)
+    df = pd.DataFrame.from_records(page_data, columns=labels)  # Taking pdf page_data and putting into a dataframe
 
     # df_deduped = df.drop_duplicates()  # Can't drop duplicates here... second page people won't get the joins...
+
+    # Splitting/Subsetting the dataframe into 3 distinct data frames for each property type
 
     df_room = df[df['RowType'] == 'Room']
     df_people = df[df['RowType'] == 'Person']
     df_status = df[df['RowType'] == 'Status']
 
-    # merge_test = pd.merge(df_people, df_room, 'inner', on=['Counter', 'Time', 'Page', 'Change_Property'])
-
-    # merge_test = pd.merge(merge_test, df_status, 'left', on=['Counter', 'Time', 'Page', 'Change_Property'])
+    # Joining the dataframes together (like SQL), by joining on a common key that identifies the individual people
 
     final_df = df_people.merge(df_room, 'inner', on=['Counter', 'Time', 'Page', 'Change_Property'])
     final_df = final_df.merge(df_status, 'left', on=['Counter', 'Time', 'Page', 'Change_Property'])
 
-    # Filter to just these 4 columns
+    # Filter to just these 4 columns (weird default names from the joining)
     final_df = final_df.filter(items=['Value_x', 'Time', 'Value_y', 'Value'])
     # Drop all duplicates
     final_df = final_df.drop_duplicates()
@@ -43,21 +43,21 @@ def build_data_frame(page_data):
     final_df = final_df[final_df['Value'] != 'Volunteer']
     # Rename Columns
     final_df.columns = ['Name', 'Time', 'Room', 'Status']
-    # Add Tab Column
+    # Add Tab Column (in order to split out people into distinct tabs by the Time and the Room properties)
     final_df['Tab'] = final_df['Time'] + ' ' + final_df['Room']
     # Replace Colons in Numbers for Tab Names (bad for tab names in excel)
     final_df['Tab'].replace(to_replace=':', value='', regex=True, inplace=True)
     # Resetting the row count
     final_df = final_df.reset_index(drop=True)
 
-    # List Unique Values in a Column #
+    # List Unique Values in a Column (get the unique names for the tabs, so that we can make them in excel)
     tabs = final_df['Tab'].unique().tolist()
     tabs_df_list = {}
 
     # Loop through each tab, and create a list of dataframes for each tab to be then added into the excel workbook
     for i, tab in enumerate(tabs):
-        df = final_df[final_df['Tab'] == tab]
-        df = df.filter(items=['Name', 'Time', 'Room'])
-        tabs_df_list["{0}".format(tab)] = df
+        df = final_df[final_df['Tab'] == tab]  # Get all records for the Time/Room combo
+        df = df.filter(items=['Name', 'Time', 'Room'])  # Just need to return the Name, Time, and Room fields
+        tabs_df_list["{0}".format(tab)] = df  # Need to figure this out again, but this adds each dataframe to a list
 
-    return tabs, tabs_df_list
+    return tabs, tabs_df_list  # The output of this process returns the unique tabs, and the collection of dataframes
