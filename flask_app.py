7fe2c9043fd0b93db62de41f67f58945dfc42517
Michael Chapman <chapman.michael.c@gmail.com>
2017-12-07 23:15:13 -0600

    Updating Flask Comments so that I can run locally to test

--- flask_app.py
+++ flask_app.py
@@ -47,6 +47,7 @@ def upload_file():
                     mimetype=EXCEL_MIMETYPE,
                     headers={"Content-Disposition": "attachment;filename=" + build_filename()})
 
+
 # Include the code below to allow it to run on a local host (test the app in a browser)
 # if __name__ == '__main__':
     # app.run()
